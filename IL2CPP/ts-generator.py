849bba3fe743702b1a83d34b4ec803d53943d1a1
Strackeror <rremilavergne@gmail.com>
2022-07-25 14:15:23 +0200

    Improve declaration generation by using generics

--- IL2CPP/ts-generator.py
+++ IL2CPP/ts-generator.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import re
+import pdb
 import json
 import os
 import sys
@@ -7,7 +9,9 @@ from collections import defaultdict
 from dataclasses import dataclass, field
 from pprint import pprint
 from string import ascii_letters, digits
+from types import new_class
 from typing import IO, Dict, List, TypedDict
+from unicodedata import name
 
 
 @dataclass
@@ -32,13 +36,25 @@ class Class:
     parent: Class | None = None
     name: str = ""
     local_name: str = ""
-    typescript_type: str = ""
     namespaces: List[str] = field(default_factory=list)
     methods: List[Method] = field(default_factory=list)
     fields: List[Field] = field(default_factory=list)
 
+    generic_count: int = 0
+
+    generic_parent: Class | None = None
+    generic_params: List[Class] = field(default_factory=list)
+
     def __repr__(self):
-        return f"{self.name}, {self.namespaces}"
+        return f"{self.name}"
+
+    def typescript_type(self):
+        if self.generic_parent:
+            template = f'<{",".join([c.typescript_type() for c in self.generic_params])}>'
+            ret = self.generic_parent.typescript_type() + template
+        else:
+            ret = ".".join(self.namespaces + [self.local_name])
+        return ret
 
 
 dump: Dict[str, Dict] = json.load(open("il2cpp_dump.json", encoding="utf-8"))
@@ -52,45 +68,49 @@ parsed_types = {}
 
 converted_types = {
     "System.Single": "number",
-   	"System.Double": "number",
-   	"System.Void": "number",
-   	"System.UInt8": "number",
-   	"System.UInt16": "number",
-   	"System.UInt32": "number",
-   	"System.UInt64": "number",
-   	"System.Int8": "number",
-   	"System.Int16": "number",
-   	"System.Int32": "number",
-   	"System.Int64": "number",
-   	"System.SByte": "number",
-   	"System.Byte": "number",
-   	"System.UByte": "number",
-   	"System.UIntPtr": "number",
-   	"System.IntPtr": "number",
-   	"System.Char": "number",
-   	"System.UChar": "number",
-   	"System.Void*": "number",
-   	"System.TypeCode": "number",
-   	"System.DateTime": "number",
-   	"System.TimeSpan": "number",
-   	"System.Boolean": "boolean",
+    "System.Double": "number",
+    "System.Void": "number",
+    "System.UInt8": "number",
+    "System.UInt16": "number",
+    "System.UInt32": "number",
+    "System.UInt64": "number",
+    "System.Int8": "number",
+    "System.Int16": "number",
+    "System.Int32": "number",
+    "System.Int64": "number",
+    "System.SByte": "number",
+    "System.Byte": "number",
+    "System.UByte": "number",
+    "System.UIntPtr": "number",
+    "System.IntPtr": "number",
+    "System.Char": "number",
+    "System.UChar": "number",
+    "System.Void*": "number",
+    "System.TypeCode": "number",
+    "System.DateTime": "number",
+    "System.TimeSpan": "number",
+    "System.Boolean": "boolean",
     "System.String": "string",
     "System.Void": "void",
     "Any": "any",
+    "unknown": "unknown",
 }
 
 for k, t in converted_types.items():
-    parsed_types[k] = Class(name=k, typescript_type=t)
+    parsed_types[k] = Class(name=k, local_name=t)
+
 
 class NamespaceTree(TypedDict):
     nodes: Dict[str, NamespaceTree]
     classes: List[Class]
 
-def namespace_tree_init() -> NamespaceTree: 
+
+def namespace_tree_init() -> NamespaceTree:
     return {
         "nodes": defaultdict(namespace_tree_init),
         "classes": []
     }
+
 
 namespace_tree = namespace_tree_init()
 
@@ -107,8 +127,12 @@ typescript_keyword = ["break", "case", "catch",
                       "this", "throw", "true",
                       "try", "typeOf", "var",
                       "void", "while", "with"]
+
+
 def valid_symbol(str: str):
     if not str:
+        return False
+    if str[0] in digits:
         return False
     if set(str).difference(ascii_letters + digits + "_"):
         return False
@@ -117,9 +141,13 @@ def valid_symbol(str: str):
         return False
     return True
 
+
 def make_valid_symbol(pstr: str):
     if not pstr:
         pstr = "__"
+
+    if pstr[0] in digits:
+        pstr = "_" + pstr
 
     for c in set(pstr):
         if c not in ascii_letters + digits + "_":
@@ -127,12 +155,10 @@ def make_valid_symbol(pstr: str):
                 pstr = pstr.replace(c, '_e_')
             else:
                 pstr = pstr.replace(c, '__')
-    
+
     if pstr in typescript_keyword:
         pstr += "_"
     return pstr
-
-
 
 
 def parseMethod(class_def: Class, method_name: str, method_entry: Dict):
@@ -161,87 +187,177 @@ def parseMethod(class_def: Class, method_name: str, method_entry: Dict):
 
     return new_method
 
+def makeNamespace(cls: Class, name_hierarchy: List[str]):
+    for n in name_hierarchy:
+        for s in n.split('.'):
+            cls.namespaces.append(make_valid_symbol(s))
+    if len(cls.namespaces) > 1:
+        if cls.namespaces[-1] in namespace_tree["nodes"]:
+            cls.namespaces[-1] += "_"
 
-parsed_count = 0
+    tree_cursor = namespace_tree
+    for k in cls.namespaces:
+        tree_cursor = tree_cursor["nodes"][k]
+    tree_cursor["classes"].append(cls)
+
+    if len(tree_cursor["classes"]) == 1:
+        cls.local_name = "T"
+    else:
+        cls.local_name = f"T{len(tree_cursor['classes'])}"
+
+def parseEnum(cls: Class, entry):
+    makeNamespace(cls, entry["name_hierarchy"] or [])
+
+    if "fields" in entry:
+        for _name, _field in entry["fields"].items():
+            default = ""
+            if "default" in _field:
+                default = _field["default"]
+            cls.fields.append(
+                Field(name=_name, type=parseClass(_field["type"]), default=default))
+
+    return cls
 
 
-def parseClass(name: str, force: bool = False):
-    global parsed_count
+
+def tryParseTemplateParam(name:str, entry: Dict):
+    if groups := re.fullmatch(r"!(\d+)", name):
+        new_class = Class(name=name, local_name=f"G{groups[1]}")
+        return new_class
+    if re.fullmatch(r"!!+\d+", name):
+        return parsed_types["Any"]
+    return None
+
+def tryPassFilter(name: str, entry: Dict):
+    if filter and (not name.startswith(filter)):
+        if "parent" in entry:
+            return parseClass(entry["parent"])
+        return parsed_types["Any"]
+    return None
+
+def tryParseArray(cls: Class, entry: Dict):
+    name_hierarchy = entry.get('name_hierarchy') or []
+    name = cls.name
+    if name_hierarchy == [""]:
+        if groups := re.fullmatch(r'(.+)(\[\])', name):
+            if not name.startswith("!"):
+                if groups[1] in dump:
+                    return parseGenericSpecialization(cls, "!0[]", [groups[1]])
+                else:
+                    return parseGenericSpecialization(cls, "!0[]", ["unknown"])
+        else:
+            return parseClassContent(cls, entry, ["System", "Array", "Other"])
+
+    if name == "!0[]":
+        cls.generic_count = 1
+        return parseClassContent(cls, entry, ["System", "Array", "Generic"])
+    return None
+
+def parseGenericSpecialization(cls: Class, generic_parent_name: str, generic_params: List[str]):
+    cls.generic_parent = parseClass(generic_parent_name, True)
+    if not cls.generic_parent.generic_count:
+        parsed_types[cls.name] = cls.generic_parent
+        return cls.generic_parent
+
+    cls.generic_params = [parseClass(n) for n in generic_params]
+    cls.namespaces = cls.generic_parent.namespaces
+    cls.local_name = cls.generic_parent.local_name
+    return cls
+
+def tryParseGeneric(cls: Class, entry: Dict):
+    name_hierarchy = entry.get('name_hierarchy') or []
+    if "generic_arg_types" in entry:
+        base_name = '.'.join(name_hierarchy)
+        generic_types: List[str] = [g["type"]
+                                    for g in entry["generic_arg_types"]]
+        if any(t != "unknown" for t in generic_types):
+            parent_name = f'{base_name}<{","* (len(generic_types)-1)}>'
+            if parent_name in dump:
+                return parseGenericSpecialization(cls, parent_name, generic_types)
+            else:
+                parsed_types[cls.name] = parsed_types["Any"]
+                return parsed_types["Any"]
+        cls.generic_count = len(generic_types)
+        return parseClassContent(cls, entry, name_hierarchy)
+    return None
+
+def tryParseEnum(cls: Class, entry: Dict):
+    if entry.get("parent") == "System.Enum":
+        return parseEnum(cls, entry)
+    return None
+
+def parseClassContent(cls: Class, entry: Dict, name_hierarchy: List[str] | None = None):
+    name_hierarchy = name_hierarchy or entry.get("name_hierarchy")
+    name_hierarchy = name_hierarchy or []
+    makeNamespace(cls, name_hierarchy)
+
+    if "parent" in entry:
+        cls.parent = parseClass(entry["parent"], force=True)
+
+    if "methods" in entry:
+        for _name, _method in entry["methods"].items():
+            if cls.generic_count and "Static" in _method["flags"]:
+                continue
+
+            new_method = parseMethod(cls, _name, _method)
+            cls.methods.append(new_method)
+    if "fields" in entry:
+        for _name, _field in entry["fields"].items():
+            default = ""
+            if "default" in _field:
+                default = _field["default"]
+            cls.fields.append(
+                Field(name=_name, type=parseClass(_field["type"]), default=default))
+    return cls
+
+
+def parseClass(name: str, force: bool = False) -> Class:
     global parsed_types
     global namespace_tree
 
     if name in parsed_types:
         return parsed_types[name]
 
-    entry = dump[name]
-    if filter and (not name.startswith(filter)) and not force:
+    if name not in dump:
+        print("Error finding type", name)
         return parsed_types["Any"]
+    
+    entry = dump[name]
+    if cls:= tryParseTemplateParam(name, entry):
+        parsed_types[name] = cls
+        return cls
 
-    if parsed_count % 100 == 0:
-        print(f"parsing {name}, parsed {parsed_count} types")
-    parsed_count += 1
+    if not force:
+        if cls := tryPassFilter(name, entry):
+            return cls
 
     new_class = Class()
     new_class.name = name
     parsed_types[name] = new_class
 
+    if cls := tryParseEnum(new_class, entry):
+        return cls
 
-    generic_param = None
-    if name.endswith("]"):
-        if name.endswith("[]"):
-            generic_param = name[:-2]
-        elif name.endswith("[,]"):
-            generic_param = name[:-3] + "_2"
-        elif name.endswith("[,,]"):
-            generic_param = name[:-4] + "_3"
-        name = "System.Array"
-    else:
-        index = name.find("<")
-        if index > 0:
-            generic_param = name[index:]
-            name = name[:index]
-            if generic_param[-1] == ">":
-                generic_param = generic_param[1:-1]
-
-    #workarounds for weird naming stuff
-    if name.endswith("."):
-        name += "__"
-    name = name.replace('`', '__')
-    name = name.replace('!', '__')
-    new_class.namespaces = name.split('.')
-    if new_class.namespaces[-1] == "System":
-        new_class.namespaces[-1] += "_"
-
-    new_class.typescript_type = '.'.join(new_class.namespaces)
-    tree_cursor = namespace_tree
-    for k in new_class.namespaces:
-        tree_cursor = tree_cursor["nodes"][k]
-    tree_cursor["classes"].append(new_class)
+    if cls:= tryParseArray(new_class, entry):
+        return cls
     
-    if generic_param:
-        new_class.local_name = make_valid_symbol(generic_param)
-    else:
-        new_class.local_name = "T"
-    new_class.typescript_type += f'.{new_class.local_name}'
+    if cls:= tryParseGeneric(new_class, entry):
+        return cls
+    
+    return parseClassContent(new_class, entry) or parsed_types["Any"]
 
-    if "parent" in entry:
-        new_class.parent = parseClass(entry["parent"], force=True)
+# Second pass for fixes
+def passClass(cls: Class):
+    if cls.parent:
+        parent = cls.parent
+        for g in cls.parent.generic_params:
+            if g == cls:
+                cls.parent = parent.parent
 
-    if "methods" in entry:
-        for _name, _method in entry["methods"].items():
-            # Avoid full collection types
-            if name == "System.Array" and "Static" in _method["flags"]:
-                continue
-            new_method = parseMethod(new_class, _name, _method)
-            new_class.methods.append(new_method)
-    if "fields" in entry:
-        for _name, _field in entry["fields"].items():
-            default = ""
-            if "default" in _field:
-                default = _field["default"]
-            new_class.fields.append(
-                Field(name=_name, type=parseClass(_field["type"]), default=default))
-    return new_class
+
+
+
+
 
 
 def write_method(file: IO, class_def: Class, method: Method, name: str):
@@ -250,20 +366,34 @@ def write_method(file: IO, class_def: Class, method: Method, name: str):
     else:
         file.write("  " + name)
 
-    file.write(f": (this: {class_def.typescript_type}")
+    file.write(f": (this: {class_def.typescript_type()}")
     for p in method.params:
-        file.write(f", {p.name}: {p.type.typescript_type}")
-    file.write(f") => {method.ret.typescript_type};\n")
+        file.write(f", {p.name}: {p.type.typescript_type()}")
+    file.write(f") => {method.ret.typescript_type()};\n")
 
 
 def write_class(file: IO, class_def: Class):
-    file.write(f"interface __{class_def.local_name} ")
+
+    template = ""
+    template_full = ""
+    if class_def.generic_count:
+        template = "<" + \
+            ",".join(f"G{i}" for i in range(class_def.generic_count)) + ">"
+        template_full = "<" + \
+            ",".join(f"G{i} = any" for i in range(
+                class_def.generic_count)) + ">"
+
+    file.write(f"interface __{class_def.local_name}{template}")
     file.write("{\n")
+    file.write(f"// {class_def.name}\n")
+    file.write(f"// {class_def.namespaces}\n")
+    file.write(f"// {dump[class_def.name].get('name_hierarchy')}\n")
+
     for f in class_def.fields:
-        if "<" in f.name:
-            file.write(f'  "{f.name}": {f.type.typescript_type},\n')
+        if valid_symbol(f.name):
+            file.write(f'  {f.name}: {f.type.typescript_type()},\n')
         else:
-            file.write(f'  {f.name}: {f.type.typescript_type},\n')
+            file.write(f'  "{f.name}": {f.type.typescript_type()},\n')
 
     method_name_map = defaultdict(list)
     method_full_map = defaultdict(list)
@@ -287,9 +417,10 @@ def write_class(file: IO, class_def: Class):
 
     file.write("}\n")
 
-    file.write(f'type {class_def.local_name} = __{class_def.local_name} ')
+    file.write(
+        f'type {class_def.local_name}{template_full} = __{class_def.local_name}{template} ')
     if class_def.parent:
-        file.write(f"& {class_def.parent.typescript_type}")
+        file.write(f"& {class_def.parent.typescript_type()}")
     file.write(";\n")
 
 
@@ -307,7 +438,7 @@ def write_enum(file: IO, class_def: Class):
 written_count = 0
 
 
-def write_tree(file:IO, name:str, tree_cursor:NamespaceTree, prev_name=""):
+def write_tree(file: IO, name: str, tree_cursor: NamespaceTree, prev_name=""):
     global written_count
     if written_count % 100 == 0:
         print(f"writing {name}, written {written_count} namespaces")
@@ -335,12 +466,23 @@ def write_tree(file:IO, name:str, tree_cursor:NamespaceTree, prev_name=""):
 def write_type_map(file: IO):
     file.write("declare type TypeMap = {\n")
     for name, cls in parsed_types.items():
-        file.write(f' "{name}": {cls.typescript_type},\n')
+        file.write(f' "{name}": {cls.typescript_type()},\n')
     file.write("}\n")
 
 
 for typename in dump:
     parseClass(typename)
+print("parsing done")
+
+for type in parsed_types:
+    passClass(parsed_types[type])
+
+print("second pass done")
+
 file = open("il2cpp.d.ts", 'w', encoding='utf-8')
+for i in range(10):
+    file.write(f"type G{i} = any;\n")
 write_tree(file, "", namespace_tree)
+
+file = open("typemap.d.ts", 'w', encoding='utf-8')
 write_type_map(file)
