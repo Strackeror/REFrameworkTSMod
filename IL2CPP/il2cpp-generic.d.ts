2f5c81a1da31760ebab58944499f9239a4c773ce
Strackeror <rremilavergne@gmail.com>
2022-07-27 19:26:02 +0200

    Remove indexing for non-value types and fix default filters

--- IL2CPP/il2cpp-generic.d.ts
+++ IL2CPP/il2cpp-generic.d.ts
@@ -2,13 +2,12 @@
 
 declare type Members<T> = { 
     [K in keyof T]: 
-      T[K] extends (this: any, ...args: infer Params) => infer ReturnType 
-        ? (this: T, ...args: Params) => ReturnType :
-      T[K] extends (this: any) => infer ReturnType
-        ? (this:T) => ReturnType :
+      T[K] extends (this: any, ...args: infer Params) => infer ReturnType ?
+        (this: T, ...args: Params) => ReturnType :
+      T[K] extends (this: any) => infer ReturnType ? 
+        (this:T) => ReturnType :
       T[K]
-  
-  }
+}
 
 declare type Inherit<T, Parent> = T & Omit<Parent, keyof (T)>
 
