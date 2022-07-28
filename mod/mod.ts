3fcc4f523d21c0d07e663199b8c99b95dada0527
Strackeror <rremilavergne@gmail.com>
2022-07-28 11:02:49 +0200

    Remove mod.ts

--- mod/mod.ts
+++
@@ -1,9 +0,0 @@
-import {snow} from "../IL2CPP/il2cpp"
-
-sdk.hook(
-  snow.player.PlayerWeaponCtrl.M.start,
-  ([_, self]) => {
-    let obj = sdk.to_managed_object(self)
-    obj._bodyConstScale = 1.0
-  }
-)
