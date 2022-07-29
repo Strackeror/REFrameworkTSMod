1cd21fa6e2b3593aab03d1ebbcbf2d3a12d8e7f6
Strackeror <rremilavergne@gmail.com>
2022-07-29 15:13:07 +0200

    Improve script

--- copy-mod.py
+++ copy-mod.py
@@ -3,12 +3,11 @@ import os
 import shutil
 
 mhrise_path=r"C:\Program Files (x86)\Steam\steamapps\common\MonsterHunterRise"
-mod = sys.argv[1]
+for mod in sys.argv[1:]:
+    os.system("npx tstl -p " + mod)
 
-os.system("npx tstl -p " + mod)
-
-shutil.copytree(
-    os.path.join(mod, "dist"),
-    os.path.join(mhrise_path, "reframework", "autorun"),
-    dirs_exist_ok=True
-)
+    shutil.copytree(
+        os.path.join(mod, "dist"),
+        os.path.join(mhrise_path, "reframework", "autorun"),
+        dirs_exist_ok=True
+    )
