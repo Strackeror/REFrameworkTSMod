a0c24d9f88ecbec369d08dcf82469c1c8d29418f
Strackeror <rremilavergne@gmail.com>
2022-07-30 23:47:38 +0200

    Improvements
    Parsing for converted types
    Improve handling for static non-symbol-able functions
    Split filters for type map and namespace tree
    Larger default namespace tree filter
    Fallback for handling enum templates
    Ignore some stub types

--- IL2CPP/il2cpp-generic.d.ts
+++ IL2CPP/il2cpp-generic.d.ts
@@ -1,13 +1,20 @@
 // Generic types used to emulate inheritance
 
+// Tag type for static functions
+declare interface Static {
+  __Static: Static;
+}
+
+type StaticThis<This, T> = This extends Static ? Static : T;
+
 declare type Members<T> = {
   [K in keyof T]: T[K] extends (
-    this: any,
+    this: infer This,
     ...args: infer Params
   ) => infer ReturnType
-    ? (this: T, ...args: Params) => ReturnType
-    : T[K] extends (this: any) => infer ReturnType
-    ? (this: T) => ReturnType
+    ? (this: StaticThis<This, T>, ...args: Params) => ReturnType
+    : T[K] extends (this: infer This) => infer ReturnType
+    ? (this: StaticThis<This, T>) => ReturnType
     : T[K];
 };
 
@@ -15,8 +22,3 @@ declare type Inherit<T, Parent> = T & Omit<Parent, keyof T>;
 declare type Indexed<Input, Output> = {
   [K in Input as K extends string | number | symbol ? K : never]: Output;
 };
-
-// Tag type for static functions
-declare interface Static {
-  __Static: Static;
-}
