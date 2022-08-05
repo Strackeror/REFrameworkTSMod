965115696950a462f493cd8de459073c0c21e235
Strackeror <rremilavergne@gmail.com>
2022-08-05 12:39:38 +0200

    Auto wirebuff pickup

--- mod/ConvenientSpiribirds/ConvenientSpiribirds.ts
+++ mod/ConvenientSpiribirds/ConvenientSpiribirds.ts
@@ -45,4 +45,26 @@ sdk.hook(
   }
 );
 
+
+sdk.hook(
+  snow.access.ObjectPopMarker.eventIntoAccessable,
+  (args) => {
+    let self = sdk.to_managed_object(args[1])
+    if (self._Category != snow.access.ObjectPopMarker.MarkerCategory.AccessTypeCreature) {
+      return;
+    }
+
+    let wirebuff = self
+      .get_Parent()
+      ["getComponent(System.Type)"](
+        snow.envCreature.EnvironmentCreatureWireBuff.T().get_runtime_type()
+      ) as snow.envCreature.EnvironmentCreatureWireBuff;
+    if (wirebuff) {
+      wirebuff.noticePopAction(snow.player.PlayerManager.Instance.getMasterPlayerID())
+    }
+    
+
+  }
+)
+
 export {};
