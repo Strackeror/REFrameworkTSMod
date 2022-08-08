a919dde9c475baa8923b9ba4729ed4c0e9c76ec7
Strackeror <rremilavergne@gmail.com>
2022-08-08 20:42:04 +0200

    Improve packaging script

--- package-mods.py
+++ package-mods.py
@@ -1,5 +1,5 @@
 import os
-import shutil
+import sys
 from zipfile import ZipFile
 
 os.chdir(os.path.dirname(__file__))
@@ -17,3 +17,15 @@ def zipfolder(foldername: str, zip_path: str):
                         filepath,
                         foldername
                     )))
+
+
+os.makedirs("output", exist_ok=True)
+if len(sys.argv) > 1:
+    mod_dirs = sys.argv[1:]
+else:
+    mod_dirs = [os.path.join("mod", f) for f in next(os.walk("mod"))[1]]
+for m in mod_dirs:
+    name = os.path.basename(m)
+    os.system(f"npx tstl -p {m}")
+    zipfolder(os.path.join(m, "dist"),
+              os.path.join("output", f"{name}.zip"))
