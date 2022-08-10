e324a9dba5d8cdcc06a7088c535ab1c2b059689c
Strackeror <rremilavergne@gmail.com>
2022-08-10 23:28:00 +0200

    Prevent auto pickup until remaining time is low

--- mod/TouchyWirebugs/TouchyWirebugs.ts
+++ mod/TouchyWirebugs/TouchyWirebugs.ts
@@ -1,10 +1,18 @@
 import { snow } from "IL2CPP/IL2CPP";
 
+// 10 seconds
+let MINIMUM_REMAINING_TIME = 10 * 60
+
 sdk.hook(
   snow.access.ObjectPopMarker.eventIntoAccessable,
   (args) => {
     let self = sdk.to_managed_object(args[1])
     if (self._Category != snow.access.ObjectPopMarker.MarkerCategory.AccessTypeCreature) {
+      return;
+    }
+
+    let player = snow.player.PlayerManager.Instance?.findMasterPlayer();
+    if (player.get_HunterWireWildNum() >= 1 && player._HunterWireNumAddTime > MINIMUM_REMAINING_TIME) {
       return;
     }
 
